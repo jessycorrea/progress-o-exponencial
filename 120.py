bedb2b67c0cff0d0d7c72138faef70cb283a9602
jessica correa <60895621+jessycorrea@users.noreply.github.com>
2020-03-03 22:12:48 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,10 @@
+#autores:1dsi3
+	#@jessy 
+	#feito em:03/03/2020
+	#instituiçao Etec
+X=0 #declaração de comando
+VALOR=2 #declaração de variavel valor
+While (x<=21):
+	valor=valor
+	x=x+1#laço de repetição
+print(valor)#exibir a variavel valor
